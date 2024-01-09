c23c1f472555a4b6d8ec119643f8afad0b4d1a6e
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-09 10:47:31 -0600

    Fix: spending money string to integer

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -7,7 +7,7 @@ class Visitor
     def initialize(name, height, spending_money)
         @name = name
         @height = height
-        @spending_money = spending_money
+        @spending_money = spending_money.delete!("$").to_i
         @preferences = []
     end
 
